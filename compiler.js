1b9fe0aaa39272f40f040a513693c43215b1abdd
Michael <michaelc@zoovy.com>
2015-07-19 11:06:13 -0700

    compiler!

--- compiler.js
+++ compiler.js
@@ -1,137 +1,128 @@
 /******************************************************************************
- * @module isomagic-pager
+ * @module isomagic-compiler
  *
- * IsoMagic extension that adds simple app capabilities, including CSS class based transitions
- * 		Currently active page has the activeClass, gets inactiveClass added when it becomes the "previous" page
- *		After the timeout has elapsed, add the activeClass to the new page, and remove both activeClass and inactiveClass from the previous page.
- *		
- * REQUIRES 
- *		isomagic-template
- *		
- * @param {string} bodySelector default:"body"
- *		a jQuery selector for the element to add body content to.  Defaults to 'body'
- * @param {int} timeout default:500
- * 		the timeout(ms) after the inactiveClass is applied to apply the activeClass
- * @param {string} activeClass default:"active"
- *		the class added to a page to make it transition in
- * @param {string} inactiveClass default:"inactive"
- *		the class added to the page to make it transition out
- * 
  * tlc:
  *		none added.
  * 
  * middleware:
- *		checkpage - checks the current page that the app is on, and if it is identical to the req.originalUrl, halts the middleware chain, otherwise, proceeds.
- *		[builder] usetemplate - {"templateid":"sometemplate"} attaches this templateid to the res
- *		[builder] setdata - {"data":{...}} attaches this data object to the res
- *		showpage - if the res has an attached templateid (data object optional), renders that template using the data into the bodySelector on the page, and sets res.handled to true
+ *
+ * config
+ *		
+ *
  *
  *****************************************************************************/
 
 (function(){
-	var extname = "pager";
+	var extname = "compiler";
 	
 	var extension = function(_app, config){
-		config.bodySelector = config.bodySelector || 'body';
-		config.timeout = config.timeout || 500;
-		config.activeClass = config.activeClass || 'active';
-		config.inactiveClass = config.inactiveClass || 'inactive';
-		var r = {
-			u : {
-				activeView : function($){
-					return $(config.bodySelector+' [data-app-uri].'+config.activeClass+':not(.'+config.inactiveClass+')');
+		if(_app.server()){
+			var fs = require('fs');
+			var cheerio = require('cheerio');
+			var TLC = require('tlc');
+			var templates = {};
+			var mediaurl = "";
+			var compilerTLC = {
+				translate : function(context){
+					var templateid = context.args('templateid');
+					var data = context.args('data');
+					var r = false;
+					if(templateid && templates[templateid]){
+						var $tag = templates[templateid].clone();
+						r = context.tlc.run($tag,data);
+						var html = cheerio.html($tag);
+						context.focus(html);
+						}
+					return r;
 					},
-				handleTransition : function($prev, $next){
-					$prev.addClass(config.inactiveClass);
-					setTimeout(function(){
-						$next.addClass(config.activeClass);
-						$prev.removeClass(config.activeClass+' '+config.inactiveClass);
-						},config.timeout);
+				imageurl : function(context){
+					var url = mediaurl;
+					
+					var w = context.args('w');
+					var h = context.args('h');
+					var b = context.args('b');
+					var name = context.args('name');
+					if(!w && !h && !b){ url +="-/"; }
+					else{
+						if(w){ url+='W'+w+'-'; }
+						if(h){ url+='H'+h+'-'; }
+						if(b){ url+='B'+b+'-'; }
+						}
+					if(url.charAt(url.length-1) == '-'){ url = url.slice(0,url.length-1); }
+					if(url.charAt(url.length-1) != '/'){ url += '/'; }
+					url+=name;
+					
+					context.focus(url);
+					return true;
 					}
-				},
+				}
+			var tlc = new TLC();			
+			tlc.addModule('compiler',compilerTLC);
+			
+			var postcss = require('postcss');
+			var cssnext = require('cssnext');
+			var processor = postcss();
+			processor.use(cssnext());
+			
+			
+			for(var i in config.instructions){
+				var current = config.instructions[i];
+				var vars = current.vars
+				switch(current.type){
+					case "css":
+						var prefix = ":root {";
+						for(var j in current.vars){prefix += '--'+j+': '+current.vars[j]+'; ';}
+						prefix += "}";
+						for(var j in current.files){
+							var f = current.files[j];
+							var contents = fs.readFileSync(f.in, 'utf-8');
+							
+							var root = postcss.parse(prefix,{from:'isomagic-compiler-'+i});
+							var styles = postcss.parse(contents,{from:f.in});
+							var out = root.append(styles).toResult().css;
+							//css compilation breaks the synchronous rules, but that's ok since it's just css.
+							processor.process(out,{from:f.in,to:f.out}).then(function(result){
+								//console.log(result);
+								fs.writeFile(f.out, result.css);
+								});
+							}
+						break;
+					case "html":
+						templates = {}; //reset templates
+						for(var j in current.compilerTemplates){
+							var contents = fs.readFileSync(current.compilerTemplates[j], 'utf-8');
+							var $ = cheerio.load(contents);
+							$('[id]').each(function(i,e){
+								templates[$(this).attr('id')] = $(this).removeAttr('id');
+								});
+							}
+						for(var j in current.files){
+							var f = current.files[j];
+							var contents = fs.readFileSync(f.in, 'utf-8');
+							var $ = cheerio.load(contents);
+							tlc.run($,current.vars,{tlcAttr:current.tlcAttr || 'data-tlc-compile'});
+							fs.writeFileSync(f.out, $.html());
+							}
+						break;
+					default:
+						console.error('isomagic-compiler: unknown instruction type encountered '+current.type);
+						break;
+					}
+				}
+			} //end if(_app.server())
+		var r = {
 			tlc : {
 				},
 			middleware : {
-				showpage : function(req,res,next){
-					// console.log('showpage');
-					res.data = res.data || {};
-					var $view;
-					var viewhtml = '<div data-app-uri="'+req.originalUrl+'" data-tlc="bind $var \'.\'; template#translate --templateid=\''+res.templateid+'\' --data=$var; apply --append;"></div>';
-					if(_app.server()){
-						var cheerio = require('cheerio');
-						$view = cheerio.load(viewhtml);
-						res.tlc.run($view, res.data);
-						$view('[data-app-uri]').addClass(config.activeClass);
-						res.$(config.bodySelector).append($view.html());
-						}
-					else{
-						$view = $(viewhtml);
-						res.tlc.run($view, res.data);
-						res.$(config.bodySelector).append($view);
-						var $prev = _app.ext.pager.u.activeView(res.$);
-						_app.ext.pager.u.handleTransition($prev, $view);
-						}
-					
-					// console.log('setting handled true');
-					res.handled = true;
-					next();
-					},
-				checkpage : function(req,res,next){
-					// console.log('checkpage');
-					if(!_app.server()){
-						var $currentPage = _app.ext.pager.u.activeView(res.$);
-						// console.log($currentPage);
-						if($currentPage.length && $currentPage.attr('data-app-uri') == req.originalUrl){
-							//halt the middleware chain.  We're already on this page.
-							}
-						else if(!$currentPage.length){
-							//This means that we're in the middle of a page transition- 
-							//let's just hold off for now by halting the middleware chain.
-							}
-						else if(res.$('[data-app-uri="'+req.originalUrl+'"]:not(.'+config.activeClass+')').length){
-							// console.log('page is already on the DOM, show it');
-							_app.ext.pager.u.handleTransition(
-								_app.ext.pager.u.activeView(res.$),
-								res.$('[data-app-uri="'+req.originalUrl+'"]')
-								);
-							//TODO trigger client middleware
-							}
-						else {
-							next();
-							}
-						}
-					else {
-						next();
-						}
-					}
 				},
 			middlewareBuilders : {
-				setdata : function(opts){
-					return function(req,res,next){
-						res.data = opts.data;
-						next();
-						};
-					},
-				usetemplate : function(opts){
-					var mwkey = ''
-					if(opts.templateid){
-						mwkey = "template_"+opts.templateid;
-						if(!mwcache[mwkey]){
-							mwcache[mwkey] = function(t){
-								return function(req,res,next){
-									// console.log('template '+t);
-									res.templateid = t; 
-									next();}
-								}(opts.templateid)
-							}
-						}
-					return mwcache[mwkey] || null;
-					}
 				}
 			}
 		var mwcache = {};
 		return r;
 		}
+		
+	
 	// Only Node.JS has a process variable that is of [[Class]] process 
 	var isNode = false;
 	try {isNode = Object.prototype.toString.call(global.process) === '[object process]';} catch(e) {}
